7f958d9736dca5b3019da223e2f06007bcd219d2
Selynna Sun <ssun098@gmail.com>
2016-03-11 13:27:42 -0800

    update audio template

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-var audio = document.getElementById("background-music");
+var audio = document.getElementById("schubert-845");
 
 if (Dolby.checkDDPlus() === true) {
-  audio.src = 'nameshitidk.mp4';
+  audio.src = 'Schubert-D845_Dolby.mp4';
 }
 
 audio.play();
+
+var audio2 = document.getElementById("tchai-1812");
+
+if (Dolby.checkDDPlus() === true ) {
+  audio.src = 'tchai-1812_Dolby.mp4';
+}
+
+audio2.play();
+
+var audio3 = document.getElementById("???");
+
+if (Dolby.checkDDPlus() === true) {
+  audio.src = '?????.mp4';
+}
+
+audio3.play();
